2ec25ee8028fbaf5c8cb1c9cb8672275ea34a522
Aamir097 <aamir729039@gmail.com>
2025-06-30 10:40:02 +0530

    upadte

---
+++ Day_2/Calculator/js/script.js
@@ -0,0 +1,14 @@
+// Calculator
+
+let a = parseInt(prompt('Enter a: '));
+let b = parseInt(prompt('Enter b: '));
+
+console.log("Sum is: ", a + b);
+
+console.log("Sub is: ", a - b);
+
+console.log("Mul is: ", a * b);
+
+console.log("Div is: ", a / b);
+
+console.log("Mod is: ", a % b);
---
+++ Day_2/Operators/js/script1.js
@@ -0,0 +1,53 @@
+// Types of Operators
+
+// 1. Arithmetic Operator
+
+// let a = 20 , b = 10;
+
+// console.log(`a + b : ${a + b}`);
+// console.log(`a - b : ${a - b}`);
+// console.log(`a * b : ${a * b}`);
+// console.log(`a / b : ${a / b}`);
+// console.log(`a % b : ${a % b}`);
+
+
+// 2 . Assignment Operator
+
+// let a = 10;
+
+// const b = 20;
+
+// console.log(a += 10);
+// console.log(a -= 10);
+// console.log(a *= 10);
+// console.log(a /= 10);
+// console.log(a %= 10);
+
+
+// 3. Comparison Operator
+
+// let a = 10, b = 20;
+
+// console.log(a == b);
+// console.log(a != b);
+// console.log(a > b);
+// console.log(a >= b);
+// console.log(a < b);
+// console.log(a <= b);
+// console.log(a === b);      Data type checked
+// console.log(a !== b);
+
+
+// 4. Logical Operator
+
+// let a = 10; b = 20;
+
+// console.log(a >= 5 && b <= 50);
+
+// console.log(a >= 5 || b <= 50);
+
+// console.log(!(a >= 50));
+
+
+
+
