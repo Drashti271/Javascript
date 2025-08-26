1972e21cd49cc5ffc71858927209254d2f880766
Drashti271 <drashtibilimoria@gmail.com>
2025-08-26 20:02:14 +0530

    update

---
+++ Day_23_Form_Validation/User_Form_Validation/script.js
@@ -0,0 +1,18 @@
+let inputs = document.querySelectorAll('.form-input');
+let errors = {};
+
+let registration = () => {
+    inputs.forEach((input) => {
+        let {name, value, checked, type} = input;
+
+        if (value == '') {
+            document.querySelector(`.${name}-message`).innerText = `${name} is required.`;
+        }
+        else if ((type == 'radio' || type == 'checkbox') && !checked) {
+            document.querySelector(`.${name}-message`).innerText = `${name} is required.`;
+        }
+        else {
+            document.querySelector(`.${name}-message`).innerText = '';
+        }
+    });
+};
